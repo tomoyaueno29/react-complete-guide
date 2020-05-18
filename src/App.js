839c788515fe8eff8666d680dcbe7bf964b570b5
tomoyaueno29 <u.t01280729@gmail.com>
2020-05-18 23:16:44 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,13 +9,19 @@ class App extends Component {
         <div className="App">
           <h1>Hi</h1>
           <p>This is really working!</p>
-          <Person />
+          <Person name="Max" age="18" />
+          <Person name="Manu" age="30"></Person>
+          <Person name="Stephanie" />
         </div>
       </React.Fragment>
 
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1',null, 'Does this work now?'))
   }
+}
+
+Person.defaultProps = {
+  age: 10
 }
 
 export default App;
