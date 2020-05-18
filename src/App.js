e20a0e3ed5942ec921b1a3517cd39a15294953ba
tomoyaueno29 <u.t01280729@gmail.com>
2020-05-18 23:53:28 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,13 +11,24 @@ class App extends Component {
     ]
   }
 
+  switchNameHandler = () => {
+    // console.log('Was Clicked!!');
+    this.setState({
+      persons: [
+        {name: 'Tomoya', age: 22},
+        {name: 'Manu', age: 30},
+        {name: 'Stephanie', age: 20}
+      ]
+    })
+  }
+
   render() {
     return (
       <React.Fragment>
         <div className="App">
           <h1>Hi</h1>
           <p>This is really working!</p>
-          <button> Switch Name </button>
+          <button onClick={this.switchNameHandler}> Switch Name </button>
           <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
           <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
           <Person name={this.state.persons[2].name}  />
