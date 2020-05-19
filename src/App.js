a11db1a037cf7cf9836ecb3c198928c338d11437
tomoyaueno29 <u.t01280729@gmail.com>
2020-05-19 10:57:59 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -34,12 +34,23 @@ class App extends Component {
 
 
   render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    }
+
+
     return (
       <React.Fragment>
         <div className="App">
           <h1>Hi</h1>
           <p>This is really working!</p>
-          <button onClick={() => this.switchNameHandler('Maxmelian')}> Switch Name </button>
+          <button 
+            style ={style}
+            onClick={() => this.switchNameHandler('Maxmelian')}> Switch Name </button>
           <Person 
           name={this.state.persons[0].name}
           age={this.state.persons[0].age}
