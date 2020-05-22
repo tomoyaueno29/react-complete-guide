0047fd68a65e743239f6147f21ba8105da2f5785
tomoyaueno29 <u.t01280729@gmail.com>
2020-05-22 15:22:13 +0900

    Update person.js

--- src/Person/person.js
+++ src/Person/person.js
@@ -1,6 +1,21 @@
 import React from 'react';
 import './Person.css'
-import Redium from 'radium';
+import styled from 'styled-components'
+
+const StyledDiv = styled.div`
+  width: 60%;
+  margin: auto;
+  border: 1px solid #eee;
+  box-shadow: 0 2px 3px #ccc;
+  padding: 16px;
+  text-align: center;
+  margin-top: 20px;
+  margin-bottom: 10px;
+
+  @media (min-width: 500px) {
+    width: 450px;
+  }
+`;
 
 const person = (props) => {
   const style = {
@@ -9,13 +24,14 @@ const person = (props) => {
     }
   };
   return (
-    <div className="Person" style={style}>
+    // <div className="Person" style={style}>
+    <StyledDiv>
       <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
       <p>{props.children}</p>
       <input type="text" onChange={props.changed} value={props.name}/>
-    </div>
+    </StyledDiv>
   )
 }
 
 
-export default Redium(person);
+export default person;
