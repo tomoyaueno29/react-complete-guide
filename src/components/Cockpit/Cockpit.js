960844dfe19e00d1a84609cd077707e93310141f
tomoyaueno29 <u.t01280729@gmail.com>
2020-05-25 16:07:39 +0900

    Create Cockpit.js

---
+++ src/components/Cockpit/Cockpit.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import classes from './Cockpit.css'
+
+const cockpit = (props) => {
+
+  const classes = []
+  let btnClass = '';
+  if(props.showPersons){
+    btnClass = classes.Red;
+  }
+
+  if(props.persons.length <= 2){
+    classes.push('red');
+  }
+  if(props.persons.length <= 1){
+    classes.push('bold')
+  } 
+  return (
+    <div className={classes.Cockpit}>
+      <h1>Hi, I'm React App</h1>
+      <p className={classes.join(' ')}>This is really working!</p>
+      <button
+        alt={props.showPersons}
+        onClick={this.togglePersonHandler}
+      > 
+        Toggle Person</button>
+    </div>
+  );
+}
+
+export default cockpit;
