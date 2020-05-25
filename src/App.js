1e717f9e38430cea296917f116ac1b3b6d9a112e
tomoyaueno29 <u.t01280729@gmail.com>
2020-05-25 14:59:00 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 import './App.css';
+import ErrorBoundary from './ErrorBoundary/ErrorBoundary'
 
 import Person from './Person/Person';
 
