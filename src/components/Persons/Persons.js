a25990dd32ceb786774f4805b2d96911c32dd024
tomoyaueno29 <u.t01280729@gmail.com>
2020-05-27 18:28:14 +0900

    Update Persons.js

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import Person from './Person/Person';
 
-const persons = (props) => props.persons.map((person, index) => {
+const persons = (props) => {
+  console.log("[Persons.js] ...rendering")
+
+  return props.persons.map((person, index) => {
     return <Person
             click={() => props.clicked(index)}
             name={person.name}
@@ -10,5 +13,5 @@ const persons = (props) => props.persons.map((person, index) => {
             changed={(event) => props.changed(event, person.id)}
           />
   });
-
+}
 export default persons;
