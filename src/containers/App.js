a9d3d63c2e6694ee0275592e51294d3f991b5e13
tomoyaueno29 <u.t01280729@gmail.com>
2020-05-27 19:10:14 +0900

    Update App.js

--- src/containers/App.js
+++ src/containers/App.js
@@ -26,8 +26,12 @@ class App extends Component {
     return state;
   }
 
+  componentWillMount(){
+    console.log("[App.js] componentWillMount");
+  }
+
   componentDidMount(){
-    console.log("[App.js] componentDidMount")
+    console.log("[App.js] componentDidMount");
   }
 
   nameChangedHandler = (event, id) => {
@@ -73,7 +77,7 @@ class App extends Component {
     let persons = null;
 
     if(this.state.showPersons) {
-      persons = <Persons  
+      persons = <Persons
             persons={this.state.persons}
             clicked={this.deletePersonHandler}
             changed={this.nameChangedHandler}
